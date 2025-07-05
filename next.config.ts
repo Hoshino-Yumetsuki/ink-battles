10755f123bfcb89aa2edf01823a628cc6f72adf4
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2025-07-06 00:11:15 +0800

    chore: 支持混淆器

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,46 @@
-import type { NextConfig } from 'next'
+import createNextJsObfuscator from 'nextjs-obfuscator'
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const obfuscatorOptions = {
+  compact: true,
+  controlFlowFlattening: true,
+  controlFlowFlatteningThreshold: 1,
+  deadCodeInjection: true,
+  deadCodeInjectionThreshold: 1,
+  debugProtection: true,
+  debugProtectionInterval: 4000,
+  disableConsoleOutput: true,
+  identifierNamesGenerator: 'hexadecimal',
+  log: false,
+  numbersToExpressions: true,
+  renameGlobals: false,
+  selfDefending: true,
+  simplify: true,
+  splitStrings: true,
+  splitStringsChunkLength: 5,
+  stringArray: true,
+  stringArrayCallsTransform: true,
+  stringArrayEncoding: ['rc4'],
+  stringArrayIndexShift: true,
+  stringArrayRotate: true,
+  stringArrayShuffle: true,
+  stringArrayWrappersCount: 5,
+  stringArrayWrappersChainedCalls: true,
+  stringArrayWrappersParametersMaxCount: 5,
+  stringArrayWrappersType: 'function',
+  stringArrayThreshold: 1,
+  transformObjectKeys: true,
+  unicodeEscapeSequence: false
 }
+
+const pluginOptions = {
+  enabled: true
+}
+
+const withNextJsObfuscator = createNextJsObfuscator(
+  obfuscatorOptions,
+  pluginOptions
+)
+
+const nextConfig = withNextJsObfuscator({})
 
 export default nextConfig
