c6f388f93b65c3e2ac4b7680d1508375abf1d268
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2026-05-18 00:34:06 +0800

    feat: 重命名 server 文件

--- rolldown.server.config.js
+++ rolldown.server.config.js
@@ -2,17 +2,17 @@ import { defineConfig } from 'rolldown'
 import pkg from './package.json' with { type: 'json' }
 
 const external = new RegExp(
-  `^(node:|${[
-    ...Object.getOwnPropertyNames(
-      pkg.devDependencies ? pkg.devDependencies : []
-    ),
-    ...Object.getOwnPropertyNames(pkg.dependencies ? pkg.dependencies : [])
-  ].join('|')})`
+  `^(node:|${[...Object.getOwnPropertyNames(pkg.devDependencies ? pkg.devDependencies : []), ...Object.getOwnPropertyNames(pkg.dependencies ? pkg.dependencies : [])].join('|')})`
 )
 
-export default defineConfig({
-  input: './src/server/start.ts',
-  output: [{ file: 'dist/server/index.js', format: 'es', minify: true }],
-  platform: 'node',
-  external
-})
+const config = {
+  input: './src/server/index.ts'
+}
+
+export default defineConfig([
+  {
+    ...config,
+    output: [{ file: 'dist/server/index.js', format: 'es', minify: true }],
+    external
+  }
+])
